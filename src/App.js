b33abbe9be47bc7d898feaa7e38c2acc491d3309
aidanbell0 <aidanbell0182@gmail.com>
2019-12-05 14:31:25 -0500

    Particle.js working. About section almost complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 
 import Nav from './components/Nav/Nav';
+import About from './components/About/About';
+
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Nav />
-        <div>
-          <div style={{"height": "400px"}}><span>Welcome!</span></div>
-          <div id="section_1" style={{"height": "500px"}}><span>Section 1</span></div>
-          <div id="section_2" style={{"height": "500px"}}><span>Section 2</span></div>
-          <div id="section_3" style={{"height": "500px"}}><span>Section 3</span></div>
+        <div className="App">
+          <div className="scroll-div" id="section_1">
+            <About />
+          </div>
+          <div className="scroll-div" id="section_2" style={{"height": "1000px"}}><span>Section 2</span></div>
+          <div className="scroll-div" id="section_3" style={{"height": "1000px"}}><span>Section 3</span></div>
         </div>
       </div>
     );
