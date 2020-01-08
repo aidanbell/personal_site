6829c084a9a212c03dda2717187137ff786940cd
aidanbell0 <aidanbell0182@gmail.com>
2020-01-08 14:17:40 -0500

    Mobile Nav Menu implimented

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ScrollspyNav from "react-scrollspy-nav";
 
 import './Home.css';
 import './Particle.css';
@@ -11,11 +12,16 @@ class Home extends Component {
           <div class="home-container">
             <p class="name">/ab/</p>
             <div class="mobile-nav">
+              <ScrollspyNav
+                scrollTargetIds={["Home", "About", "Projects", "Contact"]}
+                activeNavClass="is-active"
+                headerBackground="true"
+                scrollDuration="200"
+              />
               <ul class="home-nav">
-                <li><a href="#Home"><span>Home</span></a></li>
-                <li><a href="#About"><span>About Me</span></a></li>
-                <li><a href="#Projects"><span>Projects</span></a></li>
-                <li><a href="#Contact"><span>Contact</span></a></li>
+                <li><a class="mobile-nav-link" href="#About"><span>About Me</span></a></li>
+                <li><a class="mobile-nav-link" href="#Projects"><span>Projects</span></a></li>
+                <li><a class="mobile-nav-link" href="#Contact"><span>Contact</span></a></li>
               </ul>
             </div>
           </div>
